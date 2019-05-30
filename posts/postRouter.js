d7c1f09fa419b8da754e5b14c82d9f55055277b8
MAllen07 <mrsmallen07@gmail.com>
2019-05-30 11:39:16 -0400

    adjusted routes and added the correct endpoints - tested in postman

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -17,11 +17,74 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/:id", (req, res) => {});
+// get by ID
 
-router.delete("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
 
-router.put("/:id", (req, res) => {});
+  db.getById(id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "The post information could not be retrieved." });
+    });
+});
+
+//Delete
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.remove(id)
+    .then(id => {
+      if (id) {
+        res.status(200).json(id);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ message: "The post could not be removed." });
+    });
+});
+
+//put
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const { text } = req.body;
+  if (!text) {
+    res
+      .status(400)
+      .send({ message: "Please provide text and user_id for the post." });
+  }
+
+  db.update(id, { text })
+    .then(update => {
+      if (update) {
+        res.status(201).json(update);
+      } else {
+        res
+          .status(404)
+          .send({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "The post information could not be modified." });
+    });
+});
 
 // custom middleware
 
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,126 @@
-const express = 'express';
+const express = require("express");
 
+const db = require("./userDb.js");
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
-});
-
-router.post('/:id/posts', (req, res) => {
-
-});
-
-router.get('/', (req, res) => {
-
-});
-
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
-
-//custom middleware
-
-function validateUserId(req, res, next) {
-
+const nameHandler = (req, res, next) => {
+  const { name } = req.body;
+  if (name[0] !== name[0].toUpperCase()) {
+    res.status(400).send("Capitalize the first letter of your name.");
+  } else {
+    next();
+  }
 };
 
-function validateUser(req, res, next) {
+// Get
+router.get("/", (req, res) => {
+  db.get()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "There was an error fetching the users." });
+    });
+});
 
-};
+// Get -> user by ID
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
 
-function validatePost(req, res, next) {
+  db.getById(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with this ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "There was an error fetching the specified user." });
+    });
+});
 
-};
+// Create/Post => User
+router.post("/", nameHandler, (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ message: "Please provide a name." });
+  }
+
+  db.insert({ name })
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "There was an error adding the user." });
+    });
+});
+
+// Delete -> User
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.remove(id)
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "This user does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "There was an error deleting the user." });
+    });
+});
+
+// Update -> User
+router.put("/:id", nameHandler, (req, res) => {
+  const id = req.params.id;
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ message: "Please provide a name." });
+  }
+
+  db.update(id, { name })
+    .then(user => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "This user does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "There was an error updating the user." });
+    });
+});
+
+// Get -> All Posts by a Specific User
+router.get("/posts/:id", (req, res) => {
+  const id = req.params.id;
+
+  db.getUserPosts(id)
+    .then(userPosts => {
+      if (userPosts) {
+        res.status(200).json(userPosts);
+      } else {
+        res.status(404).json({ message: "This user has no posts." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ message: "There was an error fetching this user's posts." });
+    });
+});
 
 module.exports = router;
