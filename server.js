4c336d05878b14dea993c8cf7a2afa70a9981aa0
MAllen07 <mrsmallen07@gmail.com>
2019-05-29 21:51:16 -0400

    added custom middleware for logger, validateuserid, validateuser, validtepost

--- server.js
+++ server.js
@@ -1,15 +1,55 @@
-const express = 'express';
+const express = "express";
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+server.get("/", (req, res) => {
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
+//logger
 function logger(req, res, next) {
+  return (req, res, next) => {
+    console.log("req.body");
+    console.group("logger");
+    next();
+  };
+}
+server.use(logger());
 
-};
+//validateUserID
 
+function validateUserID(req, res, next) {
+  return (req, res, next) => {
+    console.log("req.body");
+    console.group("validateUserId");
+    next();
+  };
+}
+server.use(validateUserID());
+
+//validateUser
+
+function validateUser(req, res, next) {
+  return (req, res, next) => {
+    console.log("req.body");
+    console.group("validateUser");
+    next();
+  };
+}
+server.use(validateUser());
+
+//validatePost
+
+function validatePost(req, res, next) {
+  return (req, res, next) => {
+    console.log("req.body");
+    console.group("validatePost");
+    next();
+  };
+}
+server.us(validatePost());
+
+//export
 module.exports = server;
